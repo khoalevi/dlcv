9b80465bd8f3d7f485ccd6d8bd6e47e5f1914a64
Khoa Vo <khoa.hyper@gmail.com>
2021-09-29 21:11:45 +0700

    add minivggnet cifar10 checkpoints

---
+++ starter/minivggnet_cifar10_checkpoints.py
@@ -0,0 +1,37 @@
+from sklearn.preprocessing import LabelBinarizer
+from levi.nn.conv import MiniVGGNet
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.datasets import cifar10
+import argparse
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-z", "--checkpoint", required=True,
+                help="path to checkpoint directory")
+args = vars(ap.parse_args())
+
+print("[INFO] loading CIFAR-10 dataset...")
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+trainX = trainX.astype("float32") / 255.0
+testX = testX.astype("float32") / 255.0
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+print("[INFO] compiling network...")
+opt = SGD(lr=0.01, decay=0.01 / 40, momentum=0.9, nesterov=True)
+model = MiniVGGNet.build(32, 32, 3, 10)
+model.compile(loss="categorical_crossentropy",
+              optimizer=opt, metrics=["accuracy"])
+
+fname = os.path.sep.join([args["checkpoint"],
+                          "checkpoint-{epoch:03d}-{val_loss:.4f}.hdf5"])
+checkpoint = ModelCheckpoint(fname, monitor="val_loss", mode="min",
+                             save_best_only=True, verbose=1)
+callbacks = [checkpoint]
+
+print("[INFO] training network...")
+H = model.fit(trainX, trainY, validation_data=(testX, testY),
+              batch_size=64, epochs=40, callbacks=callbacks, verbose=2)
