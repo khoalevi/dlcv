dfc98b70546ccbb69028050c60aefb1cdb6ea201
Khoa Vo <khoa.hyper@gmail.com>
2021-10-11 09:51:07 +0700

    update hdf5 dataset writer

---
+++ extract_features.py
@@ -0,0 +1,5 @@
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from sklearn.preprocessing import LabelEncoder
